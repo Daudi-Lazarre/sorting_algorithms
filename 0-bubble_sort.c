486dd0dd8e4d0290c1fec19ebeac9325541afdf1
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-09-19 22:48:28 -0400

    Add: The 0th question

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ *bubble_sort - Algoritm bubble
+ *@array: list of numbers
+ *@size: lenght of the Array
+ *Return: Sorted array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t max;
+	int temp;
+
+	if (array == NULL || size == 0 || size == 1)
+		return;
+	if (size == 0)
+		return;
+	if (size == 1
+	    return;
+
+	for (max = size - 1; max > 0 ; max--)
+	{
+		for (i = 0; i < max; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = temp;
+				print_array(array, size);
+			}
+		}
+
+	}
+}
